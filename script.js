0f246825ac9c4f0fa111103fb57874caac8f9069
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-06 22:51:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
 //your JS code here. If required.
+function createPromise(id) {
+
+	const delay=Math.random() * 2000 + 1000;
+	return new Promise(resolve =>{
+		setTimeout(() => {
+			resolve ({id:id,time:delay/1000});
+		},delay);
+	});	
+}
+
+function populateTable(results) {
+	const output=document.getElementById('output');
+	output.innerHTML='';
+
+	results.forEach(result) => {
+	const row=document.createElement('tr');
+	const idCell=document.createElement('td');
+	idCell.textContent=`Promise ${result.id}`;
+	const timeCell=document.createElement('td');
+	timeCell.textContent=`${result.time.toFixed(2)} seconds`;
+	row.appendChild(idCell);
+	row.appendChild(timeCell);
+	output.appendChild(row);
+	});
+}
+
+document.addEventListener('DOMContentLoaded',() => {
+
+const output=document.getElementById('output');	
+const lodgingrow=document.createElement('tr');
+const lodgingCell=document.createElement('td');
+	lodgingCell.textContent='Loading...';
+	lodgingCell.colSpan=2;
+	lodgingrow.appendChild(lodgingCell);
+	output.appendChild(lodgingrow)
+
+
+	const Promise=[
+		createPromise(1);
+		createPromise(2);
+		createPromise(3);
+	];
+
+	Promise.all(promises)
+		.then(results => {
+			results.sort((a,b) => a.id - b.id);
+			populateTable(results);
+		});	
+});
+
+
+
+
+
+
+
+		
+
+
+
+
+	
+	
+}
